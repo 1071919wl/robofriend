81c275e9885245a0f0479dab820836d2b5b06d6e
William Leung <Wllm@Williams-MacBook-Pro.local>
2020-05-03 19:39:20 -0700

    added a scroll too

---
+++ build/precache-manifest.1f2a1fa75426fc4eab0e14083892111c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d93f38021ded95cbb9631c84ee9642a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "601fbcfb3b1097ae78a6",
+    "url": "/static/css/main.c9c47900.chunk.css"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/static/js/2.479e6b44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.479e6b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601fbcfb3b1097ae78a6",
+    "url": "/static/js/main.9b7b1afd.chunk.js"
+  },
+  {
+    "revision": "46b2ab5944883490d30d",
+    "url": "/static/js/runtime-main.501fdd23.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
