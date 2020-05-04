cf0139c7f37767f52cc0ec657b8237088c53d3c3
William Leung <Wllm@Williams-MacBook-Pro.local>
2020-05-03 17:02:45 -0700

    updated files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import {robots} from "./robots";
 import "./App.css";
 
 
@@ -10,10 +9,16 @@ class App extends Component {
   constructor(){
     super()
     this.state={
-      robots: robots,
+      robots: [],
       searchfield:''
     }
   }
+
+componentDidMount(){
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(users => {this.setState({robots: users})});
+}
 
   onSearchChange = (event) => {
     this.setState({searchfield: event.target.value})
@@ -21,10 +26,13 @@ class App extends Component {
 }
 
 
-  render(){
-    const filteredRobots = this.state.robots.filter(robots =>{
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    })
+render(){
+  const filteredRobots = this.state.robots.filter(robots =>{
+    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+  })
+  if(this.state.robots.length===0){
+    return<h1>Loading</h1>
+  }else {
     return(
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
@@ -32,7 +40,7 @@ class App extends Component {
         <CardList robots={filteredRobots}/>
       </div>
     );
+    }
   }
 }
-
 export default App;
